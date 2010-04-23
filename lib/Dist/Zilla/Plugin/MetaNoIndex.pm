d277d85513fa57b031cae56083f5b32e6279c53d
Mark Gardner <mjgardner@cpan.org>
2010-04-23 22:18:09 +0000

    Import of MJGARDNER/Dist-Zilla-Plugin-MetaNoIndex-1.101130 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-MetaNoIndex
    gitpan-cpan-version:      1.101130
    gitpan-cpan-path:         MJGARDNER/Dist-Zilla-Plugin-MetaNoIndex-1.101130.tar.gz
    gitpan-cpan-author:       MJGARDNER
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/MetaNoIndex.pm
+++ lib/Dist/Zilla/Plugin/MetaNoIndex.pm
@@ -1,67 +1,63 @@
+use 5.010;
+use strict;
+use warnings;
+use utf8;
+
 package Dist::Zilla::Plugin::MetaNoIndex;
-our $VERSION = '0.0300';
 
-use Moose;
-use Moose::Autobox;
-with 'Dist::Zilla::Role::MetaProvider';
-
-sub mvp_aliases {
-    return {
-        dir         => 'directories',
-        directory   => 'directories',
-        folder      => 'directories',
-        file        => 'files',
-        'package'   => 'packages',
-        module      => 'packages',
-        class       => 'packages',
-        namespace   => 'namespaces',
-    };
+BEGIN {
+    $Dist::Zilla::Plugin::MetaNoIndex::VERSION = '1.101130';
 }
 
-sub mvp_multivalue_args { qw(directories files namespaces packages) }
+# ABSTRACT: Stop CPAN from indexing stuff
 
-has directories => (
-    is          => 'rw',
-    isa         => 'ArrayRef',
-    predicate   => 'has_directories',
+use English '-no_match_vars';
+use Moose;
+use Readonly;
+with 'Dist::Zilla::Role::MetaProvider';
+
+Readonly my %ATTR_ALIAS => (
+    directory => [qw(dir directories folder)],
+    file      => ['files'],
+    package   => [qw(class module packages)],
+    namespace => ['namespaces'],
 );
 
-has files => (
-    is          => 'ro',
-    isa         => 'ArrayRef',
-    predicate   => 'has_files',
-);
+sub mvp_aliases {
+    my %aliases;
+    while ( my ( $attr, $aliases_ref ) = each %ATTR_ALIAS ) {
+        %aliases = ( %aliases, map { $ARG => $attr } @{$aliases_ref} );
+    }
+    return \%aliases;
+}
 
-has packages => (
-    is          => 'ro',
-    isa         => 'ArrayRef',
-    predicate   => 'has_packages',
-);
+sub mvp_multivalue_args { return keys %ATTR_ALIAS }
 
-has namespaces => (
-    is          => 'ro',
-    isa         => 'ArrayRef',
-    predicate   => 'has_namespaces',
-);
+for ( keys %ATTR_ALIAS ) {
+    has $ARG => (
+        is        => 'ro',
+        isa       => 'ArrayRef[Str]',
+        init_arg  => $ARG,
+        predicate => "_has_$ARG",
+    );
+}
 
 sub metadata {
     my $self = shift;
-    my %no = ();
-    if ($self->has_directories) {
-        $no{directory} = $self->directories;
-    }
-    if ($self->has_files) {
-        $no{file} = $self->files;
-    }
-    if ($self->has_packages) {
-        $no{package} = $self->packages;
-    }
-    if ($self->has_namespaces) {
-        $no{namespace} = $self->namespaces;
-    }
-    return { no_index => \%no };
+    return {
+        no_index => {
+            map { $ARG => $self->$ARG }
+                grep { my $method = "_has_$ARG"; $self->$method }
+                keys %ATTR_ALIAS
+        }
+    };
 }
 
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+=pod
 
 =head1 NAME
 
@@ -69,81 +65,85 @@ Dist::Zilla::Plugin::MetaNoIndex - Stop CPAN from indexing stuff
 
 =head1 VERSION
 
-version 0.0300
+version 1.101130
 
 =head1 SYNOPSIS
 
 In your F<dist.ini>:
 
- [MetaNoIndex]
- directory = author.t
- directory = examples
- file = lib/Foo.pm
- package = My::Module
- namespace = My::Module
+  [MetaNoIndex]
+  directory = t/author
+  directory = examples
+  file = lib/Foo.pm
+  package = My::Module
+  namespace = My::Module
 
 =head1 DESCRIPTION
 
-This plugin allows you to prevent PAUSE/CPAN from indexing files you don't want indexed. This is useful if you build test classes or example classes that are used for those purposes only, and are not part of the distribution. It does this by adding a C<no_index> block to your F<META.yml> file in your distribution.
+This plugin allows you to prevent PAUSE/CPAN from indexing files you don't
+want indexed. This is useful if you build test classes or example classes
+that are used for those purposes only, and are not part of the distribution.
+It does this by adding a C<no_index> block to your F<META.yml> file in your
+distribution.
 
-The following directives are available.
+=head1 ATTRIBUTES
 
-=over
+=head2 directory
 
-=item directory
+Exclude folders and everything in them. Example: C<author.t>.
+Aliases: C<folder>, C<dir>, C<directories>.
 
-Exclude folders and everything in them. Example: C<author.t>. Aliases: C<folder>, C<dir>.
-
-=item file
+=head2 file
 
 Exclude a specific file. Example: C<lib/Foo.pm>.
+Alias: C<files>.
 
-=item package
+=head2 package
 
-Exclude by package name. Example: C<My::Package>. Aliases: C<class>, C<module>.
+Exclude by package name. Example: C<My::Package>.
+Aliases: C<class>, C<module>, C<packages>.
 
-=item namespace
+=head2 namespace
 
 Exclude everything under a specific namespace. Example: C<My::Package>. 
+Alias: C<namespaces>.
 
-B<NOTE:> This will not exclude the package C<My::Package>, only everything under it like C<My::Package::Foo>.
+B<NOTE:> This will not exclude the package C<My::Package>, only everything
+under it like C<My::Package::Foo>.
 
-=back
+=head1 METHODS
 
-=head1 SUPPORT
+=head2 metadata
 
-=over
+Returns a reference to a hash containing the distribution's no_index metadata.
 
-=item Repository
+=encoding utf8
 
-L<http://github.com/rizen/Dist-Zilla-Plugin-MetaNoIndex>
-
-=item Bug Reports
-
-L<http://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-MetaNoIndex>
-
-=back
+=for Pod::Coverage     mvp_aliases
+    mvp_multivalue_args
 
 =head1 SEE ALSO
 
-L<Dist::Zilla>
+=over
 
-=head1 AUTHOR
+=item L<Dist::Zilla>
 
-JT Smith <jt_at_plainblack_com>
+=item L<META-spec|http://module-build.sourceforge.net/META-spec-current.html>
 
-=head1 LEGAL
+=back
 
-Dist::Zilla::Plugin::MetaNoIndex is Copyright 2010 Plain Black Corporation (L<http://www.plainblack.com/>) and is licensed under the same terms as Perl itself.
+=head1 AUTHORS
+
+  Mark Gardner <MJGARDNER@cpan.org>
+  JT Smith <RIZEN@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Mark Gardner.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
-
-
-
-
-
-
-__PACKAGE__->meta->make_immutable;
-no Moose;
-1;
+__END__
--- t/release-kwalitee.t
+++ t/release-kwalitee.t
@@ -15,7 +15,6 @@ BEGIN {
 
 use Test::More;
 
-eval "use Test::Pod 1.00";
-plan skip_all => "Test::Pod 1.00 required for testing POD" if $@;
-
-all_pod_files_ok();
+eval "use Test::Kwalitee";
+plan skip_all => "Test::Kwalitee required for testing kwalitee"
+    if $@;
